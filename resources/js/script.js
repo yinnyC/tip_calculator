9be239850ea45a01c9ae4160cc309bd84d435e82
Yin <yinnnn17@gmail.com>
2020-09-22 14:05:42 -0700

    add display totalcost and tiptotal

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,5 @@
-const display = document.querySelector('#display');
+const displayTip = document.querySelector('#display-tip-total');
+const displayTotal = document.querySelector('#display-total-cost');
 const billInput = document.querySelector('#bill');
 const tipInput = document.querySelector('#tip');
 const peopleInput = document.querySelector('#people');
@@ -7,12 +8,14 @@ function calculateTip() {
 	const billValue = parseInt(billInput.value);
 	const tipValue = parseInt(tipInput.value);
 	const pepleValue = parseInt(peopleInput.value);
-	console.log('bill is', billValue);
-	console.log('tip is', tipValue);
-	console.log('ppl is', pepleValue);
-	const tipAmount = (billValue * (tipValue / 100)) / pepleValue;
+	const tipAmount = parseFloat(
+		((billValue * (tipValue / 100)) / pepleValue).toFixed(2)
+	);
 	console.log(tipAmount);
-	display.innerHTML = tipAmount;
+	displayTip.innerHTML = tipAmount;
+	displayTotal.innerHTML = tipAmount + billValue;
+	console.log('displaytip is', displayTip);
+	console.log('displaytotal is', displayTotal);
 }
 billInput.addEventListener('input', calculateTip);
 tipInput.addEventListener('input', calculateTip);
