072b6a1d55cf2593050696ba0ae0ed2aa7621252
Yin <yinnnn17@gmail.com>
2020-09-22 11:24:17 -0700

    challenge 1

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,14 +5,12 @@ const tipInput = document.querySelector('#tip');
 console.log(display.innerHTML); // prints the text inside the tags
 console.log(billInput.value); // prints the value entered into the input
 
-billInput.value = 999.88; // Set the value to 999.88
-
 function calculateTip() {
 	const billValue = billInput.value;
 	const tipValue = tipInput.value;
 
 	const tipAmount = (billValue * tipValue) / 100;
-	display.innerHTML = tipAmount;
+	display.innerHTML = tipAmount.toFixed(2);
 }
 billInput.addEventListener('input', calculateTip);
 tipInput.addEventListener('input', calculateTip);
